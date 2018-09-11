9b6a20a35471a12e355f12fae31e3af5c6d2a7a4
Vicrobot <chaosplusorder@gmail.com>
2018-09-11 19:06:16 -0400

    Add Motion and erase backgound

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import sys, pygame
-from pygame.locals import *   # imports  all local files of the local directory
-pygame.init()                 # initiate all related internal pygame modules
+from pygame.locals import *                                # imports  all local files of the local directory
+pygame.init()                                              # initiate all related internal pygame modules
 
 
 
 size = width, height = 800, 900
 speed = [2, 2]
-black = 0, 0, 0               # RGB mode #000000 HEX_C
+black = 0, 0, 0                                            # RGB mode #000000 HEX_C
 
-screen = pygame.display.set_mode(size)            # make the screen;   (pygame.Surface object )
+screen = pygame.display.set_mode(size)                     # make the screen;   (pygame.Surface object )
 
-ball = pygame.image.load("intro_ball.gif")        
-ballrect = ball.get_rect()                        # get the rectangular coordinates of ball (pygame.Surface obj)
+play = pygame.image.load("t.png")        
+playrect = play.get_rect()                                 # get the rectangular coordinates of ball (pygame.Surface obj)
+
+back = pygame.image.load("var.jpg")
+screen.blit(back, (0,0) )
+screen.blit(play,playrect)
+
+for i in range(100):
+    pygame.time.delay(100)                                 # time delay; so that we'd be able to see changes.
+    screen.blit(back, playrect, playrect)                  # erasing the previous drawn play with placing back of same size there.
+    playrect = playrect.move(2,0)                          # updating playrect on each loop
+    screen.blit(play,playrect)
+    pygame.display.flip()
+    
 
 while 1:
-    for event in pygame.event.get():              # Take care of all events happening.
+    for event in pygame.event.get():                       # Take care of all events happening.
         if event.type == pygame.QUIT: sys.exit()  
 
-    ballrect = ballrect.move(speed)               # simply changes the coordinates
-    if ballrect.left < 0 or ballrect.right > width:     
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+   
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)                   # bitblt the pixels
-    pygame.display.flip()                         # shows all changes at last
-
-"""
 
 if __name__ == '__main__':
     main()
-"""
+
